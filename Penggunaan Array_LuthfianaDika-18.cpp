4322adb1e2b608af95491ea517c91f5b0363f31a
Luthfiana Dika <141222002+luthfianadika@users.noreply.github.com>
2023-08-09 15:18:55 +0700

    Update and rename Fungsi Round_LuthfianaDika-18.cpp to Penggunaan Array_LuthfianaDika-18.cpp

---
+++ Penggunaan Array_LuthfianaDika-18.cpp
@@ -0,0 +1,10 @@
+#include <cstdlib>
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main ()
+{
+	//mendeklarasikan array dan variabel
+       int array1 [5] = {7, 6, 9, 1, 16};
+       int array2
